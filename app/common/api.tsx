ea51cb167f4e1be8b4fa73e0ddeceed9db6a08c1
syunsuke-I <odol@ymail.ne.jp>
2024-03-17 22:12:41 +0900

    命名の変更

--- app/common/api.tsx
+++ app/common/api.tsx
@@ -1,10 +1,21 @@
 import axios from 'axios';
 
-export const instance = axios.create({
+// JSONデータの送受信を主とするAPIリクエスト用のaxiosインスタンス
+export const jsonAxiosInstance = axios.create({
   baseURL: 'http://localhost:8080',
   headers: {
     'Content-Type': 'application/json',
   },
   timeout: 2000,
   withCredentials: true,
-})
+});
+
+// ファイルアップロードなど、multipart/form-dataを扱うAPIリクエスト用のaxiosインスタンス
+export const formDataAxiosInstance = axios.create({
+  baseURL: 'http://localhost:8080',
+  headers: {
+    'Content-Type': 'multipart/form-data',
+  },
+  timeout: 2000,
+  withCredentials: true,
+});
